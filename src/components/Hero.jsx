40ba273e87d678abc0b74aa57560b439d759d5ad
rohitlodhiii <151428574+rohitlodhiii@users.noreply.github.com>
2023-12-10 20:47:25 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Rohitlodhii", "_blank")
+            window.open("https://github.com/rohitlodhiii/project_ai_summarizer-main", "_blank")
           }
           className='black_btn font-nikhil'
         >
